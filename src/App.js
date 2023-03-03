899be92eec9fbfdf540ad5babfe152fbcdb49ce9
Anthony Guzman <anthonyguzman@MacBook-Air-de-Anthony.local>
2023-03-02 21:50:12 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,16 @@ function App() {
 
   return (
     <div className={style.container}>
-   
-      
       <div className={style.spline_container}>
         <div className={style.spline}>
           <Spline scene="https://prod.spline.design/zKWaEcGOHzJcxR-P/scene.splinecode" />
         </div>
       </div>
-
       <div className={style.FormPage_container}>
         <div className={style.FormPage}>
           <FormPage />
         </div>
       </div>
-
     </div>
   );
 }
